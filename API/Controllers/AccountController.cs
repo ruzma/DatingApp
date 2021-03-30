b533c2ce4113d80efb9e26772e81fa51b1b3c844
ruzma <zajecarskopivo82@gmail.com>
2021-03-30 15:34:58 +0200

    Making a progress nicely :)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,7 @@ namespace API.Controllers
 
         [HttpPost("login")]
 
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
@@ -62,7 +62,11 @@ namespace API.Controllers
             {
                 if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
